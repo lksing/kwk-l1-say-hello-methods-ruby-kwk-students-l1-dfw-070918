0ceb4d5b1e2e2e64bb6b09d397e09b301ad69d9a
lksing <leiaxsing@gmail.com>
2018-07-11 14:27:01 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,23 @@
 # Build your say_hello method here
+
+def say_hello(name = "Ruby Programmer")
+  puts "Hello #{name}!"
+end
+
+say_hello
+
+
+
+
+
+
+
+
+def little_apple(name = '小苹果')
+  puts "你是我的小呀小 #{name}
+  怎么爱你都不嫌多"
+end
+
+little_apple()
+
+
